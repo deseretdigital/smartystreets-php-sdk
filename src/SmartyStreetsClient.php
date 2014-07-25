36ad143ca8d955dc4c8f4564c1862810b311693a
rromanch <rromanch@softserveinc.com>
2014-07-25 13:19:43 +0300

    Created SmartyStreets Client

---
+++ src/SmartyStreetsClient.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace DDM\Api\SmartyStreets;
+
+class SmartyStreetsClient
+{
+
+    protected $_authId = 'auth-id';
+
+    protected $_authToken = 'auth-token';
+
+    protected $_url = 'https://api.smartystreets.com/street-address/';
+
+    protected $_ch;
+
+    public function __construct() {
+        // Initialize cURL
+        $this->_ch = curl_init();
+
+        // Configure the cURL command
+        curl_setopt($this->_ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->_ch, CURLOPT_POST, true);
+        curl_setopt($this->_ch, CURLOPT_HEADER, 0);
+        //curl_setopt($this->_ch, CURLOPT_HTTPHEADER, array('x-standardize-only: true'));    // Enable this line if you want to only standardize addresses that are "good enough"
+        curl_setopt($this->_ch, CURLOPT_VERBOSE, 0);
+        curl_setopt($this->_ch, CURLOPT_URL, $this->_url . '?auth-id=' .$this->_authId . '&auth-token=' $this->_authToken);
+    }
+
+    public function geocode($locations) {
+        if (!is_array($locations) || count($locations)>100) {
+            $error = 'Input array is too large';
+            return $error;
+        }
+
+        $json_input = json_encode($locations);
+        curl_setopt($this->_ch, CURLOPT_POSTFIELDS, $json_input);
+
+        // Output comes back as a JSON string.
+        $json_output = curl_exec($this->_ch);
+
+
+        $coords = json_decode($json_output);
+
+        return $coords;
+    }
+
+}
