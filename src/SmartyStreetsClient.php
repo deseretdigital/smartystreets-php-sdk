c21dcba51656802b3f8043afe6b95435c3baf147
rromanch <rromanch@softserveinc.com>
2014-07-25 16:13:18 +0300

    Added simulation of response from SmartyStreets service

--- src/SmartyStreetsClient.php
+++ src/SmartyStreetsClient.php
@@ -23,7 +23,7 @@ class SmartyStreetsClient
         curl_setopt($this->_ch, CURLOPT_HEADER, 0);
         //curl_setopt($this->_ch, CURLOPT_HTTPHEADER, array('x-standardize-only: true'));    // Enable this line if you want to only standardize addresses that are "good enough"
         curl_setopt($this->_ch, CURLOPT_VERBOSE, 0);
-        curl_setopt($this->_ch, CURLOPT_URL, $this->_url . '?auth-id=' .$this->_authId . '&auth-token=' $this->_authToken);
+        curl_setopt($this->_ch, CURLOPT_URL, $this->_url . '?auth-id=' .$this->_authId . '&auth-token=' . $this->_authToken);
     }
 
     public function geocode($locations) {
@@ -36,12 +36,54 @@ class SmartyStreetsClient
         curl_setopt($this->_ch, CURLOPT_POSTFIELDS, $json_input);
 
         // Output comes back as a JSON string.
-        $json_output = curl_exec($this->_ch);
+        //$json_output = curl_exec($this->_ch);
 
+        $json_output = '
+        [
+            {
+                "input_index": 0,
+                "candidate_index": 0,
+                "addressee": "Apple Inc"
+                "delivery_line_1": "1 Infinite Loop",
+                "delivery_line_2": "PO Box 42",
+                "last_line": "Cupertino CA 95014-2083",
+                "delivery_point_barcode": "950142083017",
+                "components": {
+                    "primary_number": "1",
+                    "street_name": "Infinite",
+                    "street_suffix": "Loop",
+                    "city_name": "Cupertino",
+                    "state_abbreviation": "CA",
+                    "zipcode": "95014",
+                    "plus4_code": "2083",
+                    "delivery_point": "01",
+                    "delivery_point_check_digit": "7"
+                },
+                "metadata": {
+                    "record_type": "S",
+                    "county_fips": "06085",
+                    "county_name": "Santa Clara",
+                    "carrier_route": "C067",
+                    "congressional_district": "15",
+                    "rdi": "Commercial",
+                    "latitude": 37.33118,
+                    "longitude": -122.03062,
+                    "precision": "Zip9"
+                },
+                "analysis": {
+                    "dpv_match_code": "Y",
+                    "dpv_footnotes": "AABB",
+                    "dpv_cmra": "N",
+                    "dpv_vacant": "N",
+                    "active": "Y"
+                }
+            }
+        ]
+        ';
 
-        $coords = json_decode($json_output);
+        $results = json_decode($json_output);
 
-        return $coords;
+        return $results;
     }
 
 }
