d582e74256ec786f460a575b9629046e3cc513d4
Justin Macdonald <jmacdonald@deseretdigital.com>
2014-07-29 16:47:10 -0600

    Updates Smartystreetsclient to use Zend Http Client

--- src/SmartyStreetsClient.php
+++ src/SmartyStreetsClient.php
@@ -2,93 +2,250 @@
 
 namespace DDM\Api\SmartyStreets;
 
-class SmartyStreetsClient
+class SmartyStreetsClient extends \Zend_Http_Client
 {
 
-    protected $_authId = 'auth-id';
+    protected $lastResourcePath = null;
 
-    protected $_authToken = 'auth-token';
+    protected $baseUri = null;
 
-    protected $_url = 'https://api.smartystreets.com/street-address/';
+    protected $clientConfig = array();
 
-    protected $_ch;
+    protected $localIniKey = 'api';
 
-    public function __construct() {
-        // Initialize cURL
-        $this->_ch = curl_init();
+    protected $clientConfigParams = array(
+        'authId'    => 1,
+        'authToken' => 1
+    );
 
-        // Configure the cURL command
-        curl_setopt($this->_ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($this->_ch, CURLOPT_POST, true);
-        curl_setopt($this->_ch, CURLOPT_HEADER, 0);
-        //curl_setopt($this->_ch, CURLOPT_HTTPHEADER, array('x-standardize-only: true'));    // Enable this line if you want to only standardize addresses that are "good enough"
-        curl_setopt($this->_ch, CURLOPT_VERBOSE, 0);
-        curl_setopt($this->_ch, CURLOPT_URL, $this->_url . '?auth-id=' .$this->_authId . '&auth-token=' . $this->_authToken);
+    /**
+     * Constructor method. Will create a new HTTP client. Accepts the target
+     * URL and optionally configuration array.
+     *
+     * @param Zend_Uri_Http|string $uri
+     * @param array $config Configuration key-value pairs.
+     */
+    public function __construct($uri = null, $config = array())
+    {
+
+        // If not explicitly set use app ini settings
+        $config = $this->initClientConfig($config);
+
+        // Extract Client Config
+        $config = $this->extractClientConfig($config);
+
+        // Set default adaptor
+        if (! isset($config['adapter'])) {
+            $config['adapter'] = 'Zend_Http_Client_Adapter_Curl';
+        }
+
+        parent::__construct($uri, $config);
+
     }
 
-    public function geocode($locations) {
-        if (!is_array($locations) || count($locations)>100) {
-            $error = 'Input array is too large';
-            return $error;
+    /**
+     * Send the HTTP request and return an HTTP response object
+     *
+     * @param string $method
+     * @return Zend_Http_Response
+     * @throws Zend_Http_Client_Exception
+     */
+    public function request($method = null)
+    {
+
+        $this->injectAuthParams();
+
+        $response = parent::request($method);
+
+        return $response;
+    }
+
+    /**
+     * Adds auth id and token to request
+     */
+    public function injectAuthParams()
+    {
+        // Add in auth id and token
+        $this->setParameterGet('auth-id', $this->config['authId']);
+        $this->setParameterGet('auth-token', $this->config['authToken']);
+    }
+
+    /**
+     * Set base API uri. This is used to construct API paths
+     *
+     * @param string $uri
+     */
+    public function setBaseUri($uri)
+    {
+        $this->baseUri = $uri;
+    }
+
+    /**
+     * Return API base URI
+     */
+    public function getBaseUri()
+    {
+        return $this->baseUri;
+    }
+
+    /**
+     * Set configuration parameters for this HTTP client
+     *
+     * @param  array $config
+     * @return Local_Api_Client_Abstract
+     * @throws Zend_Http_Client_Exception
+     */
+    public function setConfig($config = array())
+    {
+        // Extract client config
+        if (count($config)) {
+            $config = $this->extractClientConfig($config);
         }
 
-        $json_input = json_encode($locations);
-        curl_setopt($this->_ch, CURLOPT_POSTFIELDS, $json_input);
+        parent::setConfig($config);
 
-        // Output comes back as a JSON string.
-        //$json_output = curl_exec($this->_ch);
+        return $this;
+    }
 
-        $json_output = '[';
-        $i = 0;
-        // We suppose that intup_id (location_id from db) will come in response from SmartyStreets service
-        foreach($locations as $location) {
-            $json_output .= '{
-                    "input_index": ' . $i . ',
-                    "candidate_index": 0,
-                    "addressee": "Apple Inc",
-                    "delivery_line_1": "1 Infinite Loop",
-                    "delivery_line_2": "PO Box 42",
-                    "last_line": "Cupertino CA 95014-2083",
-                    "delivery_point_barcode": "950142083017",
-                    "input_id": "' . $location['input_id'] . '",
-                    "components": {
-                        "primary_number": "1",
-                        "street_name": "Infinite",
-                        "street_suffix": "Loop",
-                        "city_name": "Cupertino",
-                        "state_abbreviation": "CA",
-                        "zipcode": "95014",
-                        "plus4_code": "2083",
-                        "delivery_point": "01",
-                        "delivery_point_check_digit": "7"
-                    },
-                    "metadata": {
-                        "record_type": "S",
-                        "county_fips": "06085",
-                        "county_name": "Santa Clara",
-                        "carrier_route": "C067",
-                        "congressional_district": "15",
-                        "rdi": "Commercial",
-                        "latitude": 37.33118,
-                        "longitude": -122.03062,
-                        "precision": "Zip9"
-                    },
-                    "analysis": {
-                        "dpv_match_code": "Y",
-                        "dpv_footnotes": "AABB",
-                        "dpv_cmra": "N",
-                        "dpv_vacant": "N",
-                        "active": "Y"
-                    }
-                },';
-            $i++;
+    /**
+     * Getter for config
+     * @return array
+     */
+    public function getConfig()
+    {
+
+    }
+
+    /**
+     * Build full API resource path and set clients current URI
+     *
+     * @param string $path
+     * @param string $base
+     * @return Local_Api_Client_Abstract
+     */
+    public function setApiUri($resourcePath, $base=null)
+    {
+        if (is_null($base)) {
+            $base = $this->getBaseUri();
         }
 
-        $json_output = rtrim($json_output, ',') . ']';
+        // Trim / from front of path
+        $resourcePath = ltrim($resourcePath, '/');
 
-        $results = json_decode($json_output, true);
+        // Store last resource path
+        $this->lastResourcePath = $resourcePath;
 
-        return $results;
+        $this->setUri($base. $resourcePath);
+
+        return $this;
+    }
+
+    /**
+     * Get the full URI including queryParams for the next request
+     *
+     * @param boolean $as_string If true, will return the URI as a string
+     * @return mixed Zend_Uri_Http|string
+     */
+    public function getFullUri($asString=false)
+    {
+        // Clone the URI and add the additional GET parameters to it
+        if (is_null($this->uri)) {
+            return ($asString)? 'uri not set' : $this->uri;
+        }
+
+        $uri = clone $this->uri;
+        if (! empty($this->paramsGet)) {
+            $query = $uri->getQuery();
+            if (! empty($query)) {
+                $query .= '&';
+            }
+            $query .= http_build_query($this->paramsGet, null, '&');
+            if ($this->config['rfc3986_strict']) {
+                $query = str_replace('+', '%20', $query);
+            }
+
+            $uri->setQuery($query);
+        }
+
+        if ($asString) {
+            return $uri->__toString();
+        }
+
+        return $uri;
+    }
+
+    /**
+     * Merges application ini settings with config. App ini settings only used if
+     * not explicitly set in config
+     *
+     * @param array|obj $config
+     */
+    protected function initClientConfig($config)
+    {
+        if (! isset($local[$this->localIniKey])) {
+            return $config;
+        }
+
+        return array_merge($local[$this->localIniKey], $config);
+
+    }
+
+    /**
+     * Extract client config params from general config.
+     * Client config is stored in clientConfig array
+     *
+     * @param array $config
+     * @param boolean $verifyConfig if true config will be verified
+     * @return array $config
+     * @throws Local_Api_Client_Exception
+     */
+    protected function extractClientConfig($config, $verifyConfig=true)
+    {
+
+        foreach (array_keys($this->clientConfigParams) as $key) {
+            if (isset($config[$key])) {
+                $this->clientConfig[$key] = $config[$key];
+                unset($config[$key]);
+            }
+        }
+
+        // Read in API specific config
+        if (isset($config['baseUri'])) {
+            $this->baseUri = rtrim($config['baseUri'], '/');
+            $this->baseUri .= '/';
+            $this->clientConfig['baseUri'] = $this->baseUri;
+            unset($config['baseUri']);
+        }
+
+        if ($verifyConfig) {
+            $this->verifyClientConfig();
+        }
+
+        return $config;
+    }
+
+    /**
+     * Check that all required config params are present.
+     * Probably could be turned off in production.
+     *
+     * @param array $config
+     * @throws Local_Api_Client_Exception
+     */
+    protected function verifyClientConfig()
+    {
+
+        // Iterate through client specific params
+        foreach($this->clientConfigParams as $key => $required) {
+            if ($required
+                && (! isset($this->clientConfig[$key]) || is_null($this->clientConfig[$key])) ) {
+                throw new \Exception('Missing required param: '. $key);
+            }
+        }
+
+        // Make sure that we have a base uri
+        if (is_null($this->baseUri)) {
+            throw new \Exception('Base URI cannot be null.');
+        }
     }
 
 }
